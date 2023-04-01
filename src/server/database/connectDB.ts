68d88fce6d155766302c87e8227ca243625bdbd7
Adrian Ferrari <sdadrian@gmail.com>
2023-04-01 08:14:30 -0300

    .

--- src/server/database/connectDB.ts
+++ src/server/database/connectDB.ts
@@ -4,8 +4,8 @@ dotenv.config();
 
 //PGHOST_LOCAL is for running the app with npm run dev from outside the container and connect to the postgres db inside the container for debuggin
 //PGHOST_PROD is for the app when running inside the same container as the postgres image
-const host = process.env.NODE_ENV === "production" ? process.env.PGHOST_PROD : process.env.PGHOST_LOCAL;
-const port = process.env.NODE_ENV === "production" ? process.env.PGPORT_PROD : process.env.PGPORT_LOCAL;
+const host = process.env.NODE_ENV === "production" ? process.env.PGHOST : process.env.PGHOST_LOCAL;
+const port = process.env.NODE_ENV === "production" ? process.env.PGPORT : process.env.PGPORT_LOCAL;
 
 export const pool = new Pool({
   host: host,
